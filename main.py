7775acb9875ec68eebfd30cefb64acf2701e3dd0
Kunal Kasodekar <kasodekarsunil@gmail.com>
2023-02-11 01:19:18 -0700

    Added test loop

--- main.py
+++ main.py
@@ -86,3 +86,19 @@ def train(epochs, optimizer, trainloader):
       print("Accuracy is:",acc)
 
   print('Finished Training')
+
+def test():
+  correct = 0
+  total = 0
+  with torch.no_grad():
+      for data in testloader:
+          images, labels = data
+          images=images.to(device)
+          labels=labels.to(device)
+          outputs = net(images)
+          _, predicted = torch.max(outputs.data, 1)
+          total += labels.size(0)
+          correct += (predicted == labels).sum().item()
+
+  print('Accuracy of the network on the 10000 test images: %d %%' % (
+      100 * correct / total))
